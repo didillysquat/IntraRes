564da384660eb9bc192264f51cb670198a742f2e
didillysquat@gmail.com <didillysquat@gmail.com>
2018-11-06 16:21:29 +0300

    created info_df

--- 18s_processing.py
+++ 18s_processing.py
@@ -27,12 +27,7 @@ def generate_info_df_for_samples():
     if os.path.isfile('{}/info_df.pickle'.format(os.getcwd())):
         info_df = pickle.load(open('{}/info_df.pickle'.format(os.getcwd()), 'rb'))
     else:
-        tara_data_dir = '/home/humebc/phylogeneticSoftware/SymPortal_Data/rawData/20180907_tara_pacific'
-
-
-        # info_df.colums = ['sample_name', 'fastq_fwd_file_name', 'fastq_rev_file_name', 'sample_type', 'host_phylum',
-        #                   'host_class', 'host_order', 'host_family', 'host_genus', 'host_species', 'collection_latitude',
-        #                   'collection_longitude', 'collection_data', 'collection_depth']
+        tara_data_dir = '/home/humebc/projects/tara/18s_data/18S_V9_1389F_1510R'
 
         # lets create a dict where the key will be the sample_name and the value will be dict with each of the above values
         info_collection_dict = {}
@@ -42,7 +37,7 @@ def generate_info_df_for_samples():
 
         # here we should have the info_collection_dict populated. We can now turn each of these into series and then
         # add them to the info_df
-        columns_for_df = ['sample_name', 'fastq_fwd_file_path', 'fastq_rev_file_path', 'coral_plankton', 'spp_water', 'location', 'site']
+        columns_for_df = ['sample_name', 'fastq_fwd_file_path', 'fastq_rev_file_path', 'coral_plankton', 'spp_water', 'location', 'site', 'size_fraction']
 
         info_df = create_info_df_from_info_collection_dict(columns_for_df, info_collection_dict)
 
@@ -50,7 +45,7 @@ def generate_info_df_for_samples():
 
         # here we have a dataframe with all of the samples in it
         # we can now move on to do the analysis of them.
-        # this hsould be done in a separte method
+        # this hsould be done in a separate method
     return info_df
 
 def generate_info_collection_dict(info_collection_dict, tara_data_dir):
@@ -77,14 +72,15 @@ def generate_info_collection_dict(info_collection_dict, tara_data_dir):
                             if water_type == 'CSW':
                                 parsing_dir_water_type = '{}/{}'.format(parsing_dir_sample_type, water_type)
                                 for individual in os.listdir(parsing_dir_water_type):
-                                    parsing_dir_indi = '{}/{}/S320'.format(parsing_dir_water_type, individual)
+                                    parsing_dir_indi = '{}/{}'.format(parsing_dir_water_type, individual)
+                                    for size_fraction_indi in os.listdir(parsing_dir_indi):
+                                        parsing_dir_indi_size = '{}/{}'.format(parsing_dir_indi, size_fraction_indi)
+                                        # now we are in the directory that contains the actual paired fastq.gz files for a
+                                        # given water sample
 
-                                    # now we are in the directory that contains the actual paired fastq.gz files for a
-                                    # given water sample
-
-                                    # collect the information we need
-                                    create_sample_dict(location, parsing_dir_indi, sample_type, site,
-                                                                                  water_type, info_collection_dict)
+                                        # collect the information we need
+                                        create_sample_dict(location, parsing_dir_indi_size, sample_type, site,
+                                                                                      water_type, info_collection_dict, size_fraction_indi)
 
 
                             elif water_type == 'SURFACE':
@@ -92,118 +88,149 @@ def generate_info_collection_dict(info_collection_dict, tara_data_dir):
                                 parsing_dir_water_type = '{}/{}/S320'.format(parsing_dir_sample_type, water_type)
 
                                 # collect the information we need
-                                create_sample_dict(location, parsing_dir_water_type, sample_type, site, water_type, info_collection_dict)
+                                create_sample_dict(location, parsing_dir_water_type, sample_type, site, water_type, info_collection_dict, size_fraction_indi)
 
 
         elif 'OA' in location:
-            parsing_dir_loc = '{}/{}/PLANKTON/SURFACE/S320/'.format(tara_data_dir, location)
-            # here we are already in a directory that contains the actual paried fastq.gz files
-            # SAMPLE NAME
-            sample_name = os.listdir(parsing_dir_loc)[0].split('_')[0]
-            # FWD and REV PATHS
-            files = os.listdir(parsing_dir_loc)
-            if len(files) != 2:
-                print('more than 2 files in individual\'s directory')
-                sys.exit(1)
-            fwd_found = False
-            rev_found = False
-            for file_name in files:
-                if 'R1' in file_name:
-                    fwd_path = '{}/{}'.format(parsing_dir_loc, file_name)
-                    fwd_found = True
-                elif 'R2' in file_name:
-                    rev_path = '{}/{}'.format(parsing_dir_loc, file_name)
-                    rev_found = True
-            # make sure that both the fwd and rev paths have been identified
-            if not fwd_found or not rev_found:
-                print('fwd or rev read not found')
-                sys.exit(1)
-            sample_dict = {'sample_name': sample_name,
-                           'fastq_fwd_file_path': fwd_path,
-                           'fastq_rev_file_path': rev_path,
-                           'coral_plankton': 'OA',
-                           'spp_water': 'PLANKTON',
-                           'location': location,
-                           'site': location}
-            info_collection_dict[sample_name] = sample_dict
+            parsing_dir_loc = '{}/{}/PLANKTON/SURFACE'.format(tara_data_dir, location)
+            for size_fraction_indi in os.listdir(parsing_dir_loc):
+                parsing_dir_indi = '{}/{}'.format(parsing_dir_loc, size_fraction_indi)
+
+                sample_name = os.listdir(parsing_dir_indi)[0].split('_')[0]
+                # FWD and REV PATHS
+                files = os.listdir(parsing_dir_indi)
+                if len(files) != 2:
+                    print('more than 2 files in individual\'s directory {}'.format(parsing_dir_indi))
+                    sys.exit(1)
+                fwd_found = False
+                rev_found = False
+                for file_name in files:
+                    if 'R1' in file_name:
+                        fwd_path = '{}/{}'.format(parsing_dir_indi, file_name)
+                        fwd_found = True
+                    elif 'R2' in file_name:
+                        rev_path = '{}/{}'.format(parsing_dir_indi, file_name)
+                        rev_found = True
+                # make sure that both the fwd and rev paths have been identified
+                if not fwd_found or not rev_found:
+                    print('fwd or rev read not found')
+                    sys.exit(1)
+                sample_dict = {'sample_name': sample_name,
+                               'fastq_fwd_file_path': fwd_path,
+                               'fastq_rev_file_path': rev_path,
+                               'coral_plankton': 'OA',
+                               'spp_water': 'PLANKTON',
+                               'location': location,
+                               'site': 'OA',
+                               'size_fraction': size_fraction_indi
+                               }
+                info_collection_dict[sample_name] = sample_dict
     return
 
-def create_sample_dict(location, parsing_dir, sample_type, site, water_type, info_collection_dict):
+def create_sample_dict(location, parsing_dir, sample_type, site, water_type, info_collection_dict, size_fraction='coral'):
     # SAMPLE NAME
     sample_name = os.listdir(parsing_dir)[0].split('_')[0]
     # FWD and REV PATHS
     files = os.listdir(parsing_dir)
     if len(files) != 2:
-        # we don't know why this is happening and it only happens in two samples
-        # ISLAND06/SITE01/CORAL/MILLEPORA/INDIVIDUAL029/CS4L/
-        # ISLAND06/SITE01/CORAL/MILLEPORA/INDIVIDUAL026/CS4L
-        # so for the time being we will just ignore this sample
-        return
+        print('mergin fastqs in {}'.format(parsing_dir))
+        # this method will merge the fastqs together so that 4 turn into 2.
+        merge_fastqs(files, parsing_dir)
 
-        # # sometimes there appear to be two sets of files instead of just the one
-        # # for the time being I will put these down as two seperate samples and give them names with _0 and _1
-        # if len(files) == 4:
-        #     sample_base_0 = '_'.join(files[0].split('_')[:-1])
-        #     print('more than 2 files in individual\'s directory')
-        #
-        #     fwd_found_one = False
-        #     rev_found_one = False
-        #     fwd_found_two = False
-        #     rev_found_two = False
-        #     for file_name in files:
-        #         if sample_base_0 in file_name:
-        #             if 'R1' in file_name:
-        #                 fwd_path_one = '{}/{}'.format(parsing_dir, file_name)
-        #                 fwd_found_one = True
-        #             elif 'R2' in file_name:
-        #                 rev_path_one = '{}/{}'.format(parsing_dir, file_name)
-        #                 rev_found_one = True
-        #         else:
-        #             if 'R1' in file_name:
-        #                 fwd_path_two = '{}/{}'.format(parsing_dir, file_name)
-        #                 fwd_found_two = True
-        #             elif 'R2' in file_name:
-        #                 rev_path_two = '{}/{}'.format(parsing_dir, file_name)
-        #                 rev_found_two = True
-        #     if not fwd_found_one or not rev_found_one or not fwd_found_two or not rev_found_two:
-        #         print('fwd or rev read not found')
-        #         sys.exit(1)
-        #     for i, tup in enumerate([(fwd_path_one, rev_path_one), (fwd_path_two, rev_path_two)]):
-        #
-        #         sample_dict = {'sample_name': '{}_{}'.format(sample_name, i),
-        #                        'fastq_fwd_file_path': tup[0],
-        #                        'fastq_rev_file_path': tup[1],
-        #                        'coral_plankton': sample_type,
-        #                        'spp_water': water_type,
-        #                        'location': location,
-        #                        'site': site}
-        #         info_collection_dict['{}_{}'.format(sample_name, i)] = sample_dict
-        # else:
-        #     print('weird number of files found in directory {}'.format(parsing_dir))
-        #     sys.exit(1)
-    else:
-        fwd_found = False
-        rev_found = False
+        # finally re-read in the files
+        files = os.listdir(parsing_dir)
+        if len(files) != 2:
+            print('we still gotta problem {}'.format(parsing_dir))
+            sys.exit(0)
+
+
+    fwd_found = False
+    rev_found = False
+    for file_name in files:
+        if 'R1' in file_name:
+            fwd_path = '{}/{}'.format(parsing_dir, file_name)
+            fwd_found = True
+        elif 'R2' in file_name:
+            rev_path = '{}/{}'.format(parsing_dir, file_name)
+            rev_found = True
+    # make sure that both the fwd and rev paths have been identified
+    if not fwd_found or not rev_found:
+        print('fwd or rev read not found')
+        sys.exit(1)
+    sample_dict = {'sample_name': sample_name,
+                   'fastq_fwd_file_path': fwd_path,
+                   'fastq_rev_file_path': rev_path,
+                   'coral_plankton': sample_type,
+                   'spp_water': water_type,
+                   'location': location,
+                   'site': site,
+                   'size_fraction': size_fraction}
+    info_collection_dict[sample_name] = sample_dict
+    return
+
+
+def merge_fastqs(files, parsing_dir):
+    # If we get here then there have been multiple sequencing runs for the same sample
+    # we will aim to simply merge the fastqs together insitu
+    # first get the name that we want to use (this is the one containing BG
+    one_found = False
+    two_found = False
+    for file_name in files:
+        if 'BG' in file_name and 'R1' in file_name:
+            R1_name = file_name
+            one_found = True
+        if 'BG' in file_name and 'R2' in file_name:
+            R2_name = file_name
+            two_found = True
+    if not one_found or not two_found:
+        # then this may be due to there not being a BG file
+        # so we just take the first name
         for file_name in files:
             if 'R1' in file_name:
-                fwd_path = '{}/{}'.format(parsing_dir, file_name)
-                fwd_found = True
-            elif 'R2' in file_name:
-                rev_path = '{}/{}'.format(parsing_dir, file_name)
-                rev_found = True
-        # make sure that both the fwd and rev paths have been identified
-        if not fwd_found or not rev_found:
-            print('fwd or rev read not found')
-            sys.exit(1)
-        sample_dict = {'sample_name': sample_name,
-                       'fastq_fwd_file_path': fwd_path,
-                       'fastq_rev_file_path': rev_path,
-                       'coral_plankton': sample_type,
-                       'spp_water': water_type,
-                       'location': location,
-                       'site': site}
-        info_collection_dict[sample_name] = sample_dict
-    return
+                R1_name = file_name
+                R2_name = file_name.replace('R1', 'R2')
+        print('couldnt find the right files {}'.format(parsing_dir))
+    # second unzip all of the files
+    for file_name in files:
+        subprocess.run(['gunzip', '{}/{}'.format(parsing_dir, file_name)])
+    # now we have all of the files unzipped
+    # now look at each of the files and add them to a master R1 and R2.
+    # it doesn't matter whether we add the R1 or R2 first. So long as we add the pair at the same time
+    un_files = os.listdir(parsing_dir)
+    master_fastq_R1 = []
+    master_fastq_R2 = []
+    for un_file in un_files:
+        # we should go into this if twice once for each pair
+        if 'R1' in un_file:
+            # then we can read this and its pair
+            rone_path = '{}/{}'.format(parsing_dir, un_file)
+            with open(rone_path, 'r') as f:
+                rone_file = [line.rstrip() for line in f]
+            master_fastq_R1.extend(rone_file)
+
+            # now do the same for the corresponing R2 file
+            rtwo_path = '{}/{}'.format(parsing_dir, un_file.replace('R1', 'R2'))
+            with open(rtwo_path, 'r') as f:
+                rtwo_file = [line.rstrip() for line in f]
+            master_fastq_R2.extend(rtwo_file)
+
+            # now delte the files
+            os.remove(rone_path)
+            os.remove(rtwo_path)
+    # here we have a master file for the R1 and R2. We can now write it out to the name that we have
+    rone_path_to_write = '{}/{}'.format(parsing_dir, R1_name.replace('.gz', ''))
+    with open(rone_path_to_write, 'w') as f:
+        for line in master_fastq_R1:
+            f.write('{}\n'.format(line))
+    rtwo_path_to_write = '{}/{}'.format(parsing_dir, R2_name.replace('.gz', ''))
+    with open(rtwo_path_to_write, 'w') as f:
+        for line in master_fastq_R2:
+            f.write('{}\n'.format(line))
+    # now we simply need to recompress the files
+    un_files = os.listdir(parsing_dir)
+    for un_file in un_files:
+        subprocess.run(['gzip', '{}/{}'.format(parsing_dir, un_file)])
+
 
 def create_info_df_from_info_collection_dict(columns_for_df, info_collection_dict):
     series_list = []
@@ -215,74 +242,5 @@ def create_info_df_from_info_collection_dict(columns_for_df, info_collection_dic
     info_df = pd.DataFrame.from_items([(s.name, s) for s in series_list]).T
     return info_df
 
-# This method then uses the information gained from the directory structure and moves the files to a single directory
-def write_out_stats_and_reorder_files():
-    # The purpose of this will be to produce a couple of very basic stats of how many samples
-    # we have for the different types and then to write out the files into two directories.
-    # We will write out the coral samples into one directory and the water samples into another.
-    # We do this as the coral samples will go all the way through the SP analysis and have predicted.
-    # The water samples can just be submitted to the db and we can get the sequences through QC. They
-    # will still need further processing but this will be more tertiary analysis.
 
-
-    info_df = generate_info_df_for_samples()
-
-
-
-    coral_output_dir = '/home/humebc/phylogeneticSoftware/SymPortal_Data/rawData/20181014_tara_initial_corals'
-
-    non_coral_output_dir = '/home/humebc/phylogeneticSoftware/SymPortal_Data/rawData/20181014_tara_initial_non_corals'
-
-    # get the coral rows
-    coral = info_df.loc[info_df['coral_plankton'] == 'CORAL']
-    num_coral = len(coral.index.values.tolist())
-
-    # get the OA
-    OA = info_df.loc[info_df['coral_plankton'] == 'OA']
-    num_oa = len(OA.index.values.tolist())
-
-    # CSW
-    csw = info_df.loc[info_df['spp_water'] == 'CSW']
-    num_csw = len(csw.index.values.tolist())
-
-    # surface
-    surface = info_df.loc[info_df['spp_water'] == 'SURFACE']
-    num_surface = len(surface.index.values.tolist())
-
-    # # num islands
-    # num_islands = len(set([loc for loc in info_df.loc[:,'location'].values.toslist() if 'ISLAND' in loc]))
-
-    # let's now move the samples to their new directories os.rename is the command for this
-
-    #coral
-    move_files_to_new_dir(coral_output_dir, coral)
-
-    #oa
-    move_files_to_new_dir(non_coral_output_dir, OA)
-
-    #CSW
-    move_files_to_new_dir(non_coral_output_dir, csw)
-
-    #surface
-    move_files_to_new_dir(non_coral_output_dir, surface)
-
-
-
-    apples = 'asdf'
-
-    return
-
-def move_files_to_new_dir(new_dir_base, df ):
-    for ind in df.index.values.tolist():
-        indi_series = df.loc[ind]
-        fwd_file_name = indi_series['fastq_fwd_file_path'].split('/')[-1]
-        fwd_current_path = indi_series['fastq_fwd_file_path']
-        fwd_destination_path = '{}/{}'.format(new_dir_base, fwd_file_name)
-
-        os.rename(fwd_current_path, fwd_destination_path)
-
-        rev_file_name = indi_series['fastq_rev_file_path'].split('/')[-1]
-        rev_current_path = indi_series['fastq_rev_file_path']
-        rev_destination_path = '{}/{}'.format(new_dir_base, rev_file_name)
-
-        os.rename(rev_current_path, rev_destination_path)
+generate_info_df_for_samples()
